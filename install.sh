bac0c43b888410ebfcb980ddab3937d742963680
nikhil <nikhil@nikhil.cc>
2014-09-28 20:15:11 -0700

    Write install.sh to symlink configs

---
+++ install.sh
@@ -0,0 +1,78 @@
+# Expect arguments <platform> and <profile>
+# that identify a directory ./configs/<platform>/<profile>/
+
+
+### Process input
+
+if [ $# -ne 2 ]; then
+   echo "Error in $0 $@: expected 2 arguments, received $#"
+   exit 1
+fi
+
+THIS_REPO="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CONFIGS="$THIS_REPO/configs"
+PLATFORM_CONFIGS="$CONFIGS/platform_$1"
+PLATFORM_PROFILE_CONFIGS="$PLATFORM_CONFIGS/profile_$2"
+
+if [ ! -d "$PLATFORM_PROFILE_CONFIGS" ]; then
+   echo "Error in $0 $@: can't find $PLATFORM_PROFILE_CONFIGS/"
+   exit 1
+fi
+
+
+### Register configs from ./configs/<platform>/<profile>/
+
+# Make sure the wildcard returns hidden files.
+shopt -s dotglob
+# Make sure the wildcard works even when the directory is empty.
+shopt -s nullglob
+
+for filename in "$CONFIGS"/*; do
+   # Skip platform_* entries, which are special (see below).
+   if [[ `basename "$filename"` == platform_* ]]; then
+       continue
+   fi
+   # Skip temporary files.
+   git ls-files "$filename" --error-unmatch &> /dev/null
+   exit_status=$?
+   if [ $exit_status != 0 ]; then
+       continue
+   fi
+   # Create a symlink from ~/ directly to the file.
+   # DANGER: existing entries in ~/ will be lost forever!
+   linkname=~/`basename "$filename"`
+   echo "$linkname ----------------> $filename"
+   ln -sf "$filename" "$linkname"
+done
+
+for filename in "$PLATFORM_CONFIGS"/*; do
+   # Skip profile_* entries, which are special (see below).
+   if [[ `basename "$filename"` == profile_* ]]; then
+       continue
+   fi
+   # Skip temporary files.
+   git ls-files "$filename" --error-unmatch &> /dev/null
+   exit_status=$?
+   if [ $exit_status != 0 ]; then
+       continue
+   fi
+   # Create a symlink from ~/ directly to the file.
+   # DANGER: existing entries in ~/ will be lost forever!
+   linkname=~/`basename "$filename"`
+   echo "$linkname ----------------> $filename"
+   ln -sf "$filename" "$linkname"
+done
+
+for filename in "$PLATFORM_PROFILE_CONFIGS"/*; do
+   # Skip temporary files.
+   git ls-files "$filename" --error-unmatch &> /dev/null
+   exit_status=$?
+   if [ $exit_status != 0 ]; then
+       continue
+   fi
+   # Create a symlink from ~/ directly to the file.
+   # DANGER: existing entries in ~/ will be lost forever!
+   linkname=~/`basename "$filename"`
+   echo "$linkname ----------------> $filename"
+   ln -sf "$filename" "$linkname"
+done
